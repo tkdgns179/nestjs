7e0fb53b6614367479db07214435c2e231a964db
tkdgns179 <tkdgns179@gmail.com>
2024-02-27 00:00:23 +0900

    UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
 import { UserDto } from 'src/dto/user-dto';
 import { User } from 'src/entities/user.entity';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
 
 
 @Injectable()
 export class UserService {
     constructor(
         @InjectRepository(User)
-        private userRepository: Repository<User>
+        private userRepository: Repository<User>,
     ) {}
+
+    public async findUserByUserName(userName: string) : Promise<User> {
+        const user: User = await this.userRepository.createQueryBuilder()
+        .select('user')
+        .from(User, 'user')
+        .where('user.name = :userName', {userName})
+        .getOne()
+
+        return user;
+    }
+
+    public async hasUserWithNamePassword(userName: string, userPasswd: string) : Promise<boolean> {
+
+        const user = await this.findUserByUserName(userName)
+        const isMatch = await bcrypt.compare(userPasswd, user.passwd);
+        
+        return isMatch;
+    }
 
     public async saveUser(userDto: UserDto) {
         
